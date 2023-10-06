c45766df50ac5f4300c214abda619085b785388a
nic-foma <127281951+nic-foma@users.noreply.github.com>
2023-10-06 13:56:25 +1100

    Removing footer text

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -476,20 +476,6 @@ export const ChatInput = ({ onSend, onRegenerate, textareaRef }: Props) => {
           <ChatInputTokenCount content={content} />
         </div>
       </ChatInputContainer>
-      <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-        <a
-          href="https://github.com/dotneet/smart-chatbot-ui"
-          target="_blank"
-          rel="noreferrer"
-          className="underline"
-        >
-          Smart ChatBot UI
-        </a>
-        .{' '}
-        {t(
-          "Smart Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
-        )}
-      </div>
     </div>
   );
 };
